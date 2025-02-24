f82b874da538fd9a3732173198f4e8568b60de90
Dhruv Balchandani <119007749+dhruvx19@users.noreply.github.com>
2025-02-25 02:40:46 +0530

    updates

--- adhd_tracker/lib/ui/search.dart
+++
@@ -1,8 +0,0 @@
-import 'package:adhd_tracker/ui/home/goals/goals.dart';
-import 'package:adhd_tracker/ui/home/record/symptom.dart';
-import 'package:adhd_tracker/ui/home/reminder/show_reminder.dart';
-import 'package:adhd_tracker/ui/settings/settings.dart';
-import 'package:flutter/material.dart';
-
-
-
